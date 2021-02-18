e3c40e546c959a895b7d45af08ceaedfe3edfe2f
Bejean <dominique.bejean@eolya.fr>
2021-02-18 10:52:15 +0100

    Ajout de la variable SOLR_EXPORTER_METRIC_GROUPS

--- docker/solr/solr-exporter-entrypoint.sh
+++ docker/solr/solr-exporter-entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-sed -i '/name="group"/c\<str name="group">jvm,node</str>' /opt/solr/contrib/prometheus-exporter/conf/solr-exporter-config.xml
+
+echo "SOLR_EXPORTER_METRIC_GROUPS = $SOLR_EXPORTER_METRIC_GROUPS"
+if [ ! "x$SOLR_EXPORTER_METRIC_GROUPS" == "all" ] && [ ! "x$SOLR_EXPORTER_METRIC_GROUPS" == "x" ]; then
+	echo "Updating solr-exporter-config.xml"
+	sed -i "/name=\"group\"/c\<str name=\"group\">${SOLR_EXPORTER_METRIC_GROUPS}</str>" /opt/solr/contrib/prometheus-exporter/conf/solr-exporter-config.xml
+fi
+echo "Starting Solr exporter"
 /opt/solr/contrib/prometheus-exporter/bin/solr-exporter -p 9854 -z ${ZK_HOSTS}${ZK_CHROOT} -f /opt/solr/contrib/prometheus-exporter/conf/solr-exporter-config.xml -n 2
